bd3aea3fdb7a07a24e85cff6a8de59ff5f89635d
Max McDonough <max.mcdonough@gmail.com>
2022-11-19 14:57:52 -0500

    Added timeout to delay adjacent squares

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -33,100 +33,51 @@ export default function Game() {
     setBValues(Array(totalSquares).fill(255));
   }, []);
 
-  console.log(boardWidth);
-  console.log(boardHeight);
-
-  // const initialColorValues = Array(8000).fill(255);
-
-  // const [stepNumber, setStepNumber]= useState(0);
-  // const [history, setHistory] = useState([initialColorValues]);
-  // const [currentSquareColors, setCurrentSquareColors] = useState(initialColorValues);
-
-  // useEffect(() => {
-  //   setCurrent(history[history.length - 1]);
-  //   console.log(current);
-  // }, [history]);
-
+  // Set Values of Neighboring Squares
   useEffect(() => {
     if (freshClick) {
-      console.log(`fresh click: ${freshClick}`);
-      // Set array of squares to check
-      const adjacentSquares = [(freshClick - boardWidth), (freshClick + boardWidth)];
-      if (freshClick % boardWidth !== 0) {adjacentSquares.push(freshClick - 1)};
-      if ((freshClick+1) % boardWidth !== 0) {adjacentSquares.push(freshClick + 1)};
-      console.log(`adjacentSquares = ${adjacentSquares}`);
-      // Set Red
-      const updatedRed = rValues.slice();
-      adjacentSquares.map((position) => {
-        const value = updatedRed[position];
-        if (value < 255) {
-          const newValue = (value)/(1.5)
-          return newValue;
-        } else return value;
-      });
-      setRValues(updatedRed);
-      // Set Green
-      const updatedGreen = gValues.slice();
-      adjacentSquares.map((position) => {
-        const value = updatedGreen[position];
-          const newValue = (value)/(1.5)
-          return updatedGreen[position] = newValue;
-      });
-      setGValues(updatedGreen);
-      // Set Blue
-      const updatedBlue = bValues.slice();
-      adjacentSquares.map((position) => {
-        const value = updatedBlue[position];
-          const newValue = (value)/(1.5)
-          return updatedBlue[position] = newValue;
-      });
-      setBValues(updatedBlue);
-
-      // Check for squares adjacent to click
-
-      // const updatedRed = rValues.map((value, index) => {
-      //   if (( index === freshClick-1 && freshClick % boardWidth !== 0 ) ||
-      //       ( index === freshClick+1 && (freshClick+1) % boardWidth !== 0 ) ||
-      //       ( index === freshClick-boardWidth ) || 
-      //       ( index === freshClick+boardWidth )) {
-      //     console.log(`Rvalue = ${value}`);
-      //     if (value < 255) {
-      //       const newValue = (value)/(1.5)
-      //       return newValue;
-      //     } else return value;
-      //   } else return value;
-      // });
-      // setRValues(updatedRed);
-
-      // const updatedGreen = gValues.map((value, index) => {
-      //   if (( index === freshClick-1 && freshClick % boardWidth !== 0 ) ||
-      //       ( index === freshClick+1 && (freshClick+1) % boardWidth !== 0 ) ||
-      //       ( index === freshClick-boardWidth ) || 
-      //       ( index === freshClick+boardWidth )) {
-      //     console.log(`Gvalue = ${value}`);
-      //     const newValue = (value)/(1.5)
-      //     return newValue;
-      //   } else return value;
-      // });
-      // setGValues(updatedGreen);
-      
-      // const updatedBlue = bValues.map((value, index) => {
-      //   if (( index === freshClick-1 && freshClick % boardWidth !== 0 ) ||
-      //       ( index === freshClick+1 && (freshClick+1) % boardWidth !== 0 ) ||
-      //       ( index === freshClick-boardWidth ) || 
-      //       ( index === freshClick+boardWidth )) {
-      //     console.log(`Bvalue = ${value}`);
-      //     const newValue = (value)/(1.5)
-      //     return newValue;
-      //   } else return value;
-      // });
-      // setBValues(updatedBlue);
-      setFreshClick(null);
+      setTimeout(() => {
+        const sq = freshClick;
+        const bw = boardWidth;
+        console.log(`fresh click: ${sq}`);
+        // Set array of squares to check
+        const adjacentSquares = [(sq-bw), (sq-bw-1), (sq-bw+1), (sq+bw), (sq+bw-1), (sq+bw+1)];
+        if (sq % bw !== 0) {adjacentSquares.push(sq - 1)};
+        if ((sq+1) % bw !== 0) {adjacentSquares.push(sq + 1)};
+        console.log(`adjacentSquares = ${adjacentSquares}`);
+        // Set Red
+        const updatedRed = rValues.slice();
+        adjacentSquares.map((position) => {
+          const value = updatedRed[position];
+          if (value < 255) {
+            const newValue = (value)/(1.5)
+            return newValue;
+          } else return value;
+        });
+        setRValues(updatedRed);
+        // Set Green
+        const updatedGreen = gValues.slice();
+        adjacentSquares.map((position) => {
+          const value = updatedGreen[position];
+            const newValue = (value)/(1.5)
+            return updatedGreen[position] = newValue;
+        });
+        setGValues(updatedGreen);
+        // Set Blue
+        const updatedBlue = bValues.slice();
+        adjacentSquares.map((position) => {
+          const value = updatedBlue[position];
+            const newValue = (value)/(1.5)
+            return updatedBlue[position] = newValue;
+        });
+        setBValues(updatedBlue);
+        // Reset freshClick 
+        setFreshClick(null);
+      }, 100);
     };
   }, [freshClick]);
 
-
-
+  // Set Values of Clicked Square
   const handleClick = (i) => {
     console.log(`square ${i} clicked`);
     // Set Red
